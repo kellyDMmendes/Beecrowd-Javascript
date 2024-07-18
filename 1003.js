94b83f1f76b823e4cee2b506dd1a0fb84c1acfed
kellyDMmendes <152336851+kellyDMmendes@users.noreply.github.com>
2024-07-17 21:11:15 -0300

    Create 1003.js

---
+++ 1003.js
@@ -0,0 +1,9 @@
+/* 1003 - Soma Simples */
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const soma = (a, b) => a + b;
+ 
+const resultado = lines.map(Number).reduce(soma);
+
+console.log (`SOMA = ${resultado}`)
